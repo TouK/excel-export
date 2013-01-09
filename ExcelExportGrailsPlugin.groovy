decc806d260ab8f6f913efba7a22ff6db0f981aa
Jakub Nabrdalik <jakubn@gmail.com>
2013-01-09 12:07:12 +0100

    minimal grails version changed to 2.0, because this plugin requires groovy in 1.8 at least

--- ExcelExportGrailsPlugin.groovy
+++ ExcelExportGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class ExcelExportGrailsPlugin {
     // the plugin version
     def version = "0.1.1"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.1 > *"
+    def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
