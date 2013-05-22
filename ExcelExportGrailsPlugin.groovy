4a67dea4285c0430d0a5769b8513672ef0a2e82b
Jakub Nabrdalik <jakubn@gmail.com>
2013-05-22 13:27:55 +0200

    release 0.1.5

--- ExcelExportGrailsPlugin.groovy
+++ ExcelExportGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class ExcelExportGrailsPlugin {
     // the plugin version
-    def version = "0.1.4"
+    def version = "0.1.5"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
